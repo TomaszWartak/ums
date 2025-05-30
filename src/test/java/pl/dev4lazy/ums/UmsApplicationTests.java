4cfc017078ecead31adab10a9e24aa98cd4d1d63
Tomasz Wartak <tomasz.wartak@gmail.com>
2025-05-30 22:20:13 +0200

    Commit inicjujący

---
+++ src/test/java/pl/dev4lazy/ums/UmsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dev4lazy.ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
