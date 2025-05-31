6e2f2fee8b0fcc03b4da90721fb8c33035b8f9fb
Tomasz Wartak <tomasz.wartak@gmail.com>
2025-05-31 17:36:59 +0200

    cd SpringDataUserJPA.

--- src/main/java/pl/dev4lazy/ums/adapters/outbound/persistence/SpringDataUserJPA.java
+++ src/main/java/pl/dev4lazy/ums/adapters/outbound/persistence/SpringDataUserJPA.java
@@ -2,8 +2,9 @@ package pl.dev4lazy.ums.adapters.outbound.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.dev4lazy.ums.adapters.outbound.persistence.entity.UserEntity;
 
 @Repository
 public interface SpringDataUserJPA extends JpaRepository<UserEntity, Long> {
-    // tutaj możesz dodać metody typu: boolean existsByEmail(String email);
+    // todo tutaj możesz dodać metody typu: boolean existsByEmail(String email);
 }
